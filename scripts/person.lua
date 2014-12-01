a08dc542161f2f59dab8f9da90eccfc15c369975
brycehenley <brycehenley97@gmail.com>
2014-11-30 19:19:55 -0700

    fixed character controller and physics

--- scripts/person.lua
+++ scripts/person.lua
@@ -1,7 +1,8 @@
-feet = getObject("Feet")
+player = getObject("Feet")
 camera = getObject("Camera1")
 bob = getObject("bob")
 head = getObject("head")
+pblock = getObject("Entity0")
 
 dx = 0.0
 dy = 0.0
@@ -19,35 +20,52 @@ function onSceneUpdate()
 	dx = getAxis("MOUSE_X") - mx
 	dy = getAxis("MOUSE_Y") - my
 	
-	rotate(feet, {0.0, 0.0, -1.0}, dx*250.0, "local")
+	rotate(player, {0.0, 0.0, -1.0}, dx*250.0, "local")
 	rotate(head, {-1.0, 0.0, 0.0}, dy*150.0, "local")
 
 
-	rotation = getRotation(feet)
+	rotation = getRotation(player)
 	rotation[1] = 0.0
 	rotation[2] = 0.0
 
 
-	setRotation(feet, rotation)
+	setRotation(player, rotation)
 
 	
 	centerCursor()
 	hideCursor()
 
-	-- setRotation(feet, {0.0, 0.0, getRotation(feet)[3]})
+	-- CHARACTER CONTROLER
+	if isCollisionBetween(player, pblock) == true then
+		if isKeyPressed("W") then translate(player, {0.0, 1.0, 0.0}, "local") end
+		if isKeyPressed("S") then translate(player, {0.0, -1.0, 0.0}, "local") end
+		if isKeyPressed("A") then translate(player, {-1.0, 0.0, 0.0}, "local") end
+		if isKeyPressed("D") then translate(player, {1.0, 0.0, 0.0}, "local") end
+		if isKeyPressed("LSHIFT") then translate(player, {0.0, 0.25, 0.0}, "local") end
+	end
 
-	-- if isKeyPressed("W") then translate(feet, {0.0, 0.3, 0.0}, "local") end
-	-- if isKeyPressed("S") then translate(feet, {0.0, -0.3, 0.0}, "local") end
-	-- if isKeyPressed("A") then translate(feet, {-0.3, 0.0, 0.0}, "local") end
-	-- if isKeyPressed("D") then translate(feet, {0.3, 0.0, 0.0}, "local") end
+	if isKeyPressed("SPACE") then 
 
-	if isKeyPressed("W") then addCentralForce(feet, {0.0, 2000.0, 0.0}, "local") end
-	if isKeyPressed("LSHIFT") then addCentralForce(feet, {0.0, 2000.0, 0.0}, "local") end
-	if isKeyPressed("S") then addCentralForce(feet, {0.0, -2000.0, 0.0}, "local") end
-	if isKeyPressed("A") then addCentralForce(feet, {-1000.0, 0.0, 0.0}, "local") end
-	if isKeyPressed("D") then addCentralForce(feet, {1000.0, 0.0, 0.0}, "local") end
-    -- if isKeyPressed("SPACE") then addCentralForce(feet, {0.0, 0.0, 200.0}, "local") end
-  
+    	if isCollisionBetween(player, pblock) == true then 
+    		-- translate(player, {0.0, 0.0, 2.0}, "local")
+    		addCentralForce(player, {0.0, 0.0, 200.0}, "global")
+    	end
+	end
+
+    setGravity({0.0, 0.0, 0.0})
+
+    if isCollisionBetween(player, pblock) ~= true then 
+    	addCentralForce(player, {0.0, 0.0, -10.0}, "global")
+    end
+
+	-- if isKeyPressed("W") then addCentralForce(player, {0.0, 2000.0, 0.0}, "local") end
+	-- if isKeyPressed("LSHIFT") then addCentralForce(player, {0.0, 2000.0, 0.0}, "local") end
+	-- if isKeyPressed("S") then addCentralForce(player, {0.0, -2000.0, 0.0}, "local") end
+	-- if isKeyPressed("A") then addCentralForce(player, {-1000.0, 0.0, 0.0}, "local") end
+	-- if isKeyPressed("D") then addCentralForce(player, {1000.0, 0.0, 0.0}, "local") end
+    -- if isKeyPressed("SPACE") then addCentralForce(player, {0.0, 0.0, 200.0}, "local") end
+  	
+  	-- ANIMATION CONTROL
 	if onKeyDown("W") then
 		changeAnimation(bob, "3")
 	end
