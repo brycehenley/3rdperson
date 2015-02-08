46d54bae8f193aa015ab567a26cbf33d226319d6
brycehenley <brycehenley97@gmail.com>
2015-02-07 17:39:46 -0700

    camera tilt work

--- scripts/person.lua
+++ scripts/person.lua
@@ -1,9 +1,13 @@
+dofile("SDK/Neo3D.lua")
+dofile("SDK/Graphics2D.lua")
+
 player = getObject("Feet")
 camera = getObject("Camera1")
 bob = getObject("bob")
 head = getObject("head")
 pblock = getObject("Entity0")
 
+isKeyInput = false
 dx = 0.0
 dy = 0.0
 
@@ -12,6 +16,7 @@ window = getWindowScale()
 mx = getAxis("MOUSE_X")
 my = getAxis("MOUSE_Y")
 
+setGravity({0.0, 0.0, 0.0})
 
 function onSceneUpdate()
 
@@ -20,43 +25,56 @@ function onSceneUpdate()
 	dx = getAxis("MOUSE_X") - mx
 	dy = getAxis("MOUSE_Y") - my
 	
-	rotate(player, {0.0, 0.0, -1.0}, dx*250.0, "local")
+	rotate(bob, {0.0, 0.0, -1.0}, dx*250.0, "local")
+	setRotation(player, getRotation(bob), "local")
 	rotate(head, {-1.0, 0.0, 0.0}, dy*150.0, "local")
 
-
-	rotation = getRotation(player)
-	rotation[1] = 0.0
-	rotation[2] = 0.0
-
-
-	setRotation(player, rotation)
-
-	
 	centerCursor()
 	hideCursor()
 
 	-- CHARACTER CONTROLER
 	-- if isCollisionBetween(player, pblock) == true then
-		if isKeyPressed("W") then translate(player, {0.0, 0.5, 0.0}, "local") end
-		if isKeyPressed("S") then translate(player, {0.0, -0.5, 0.0}, "local") end
-		if isKeyPressed("A") then translate(player, {-0.5, 0.0, 0.0}, "local") end
-		if isKeyPressed("D") then translate(player, {0.5, 0.0, 0.0}, "local") end
-		if isKeyPressed("LSHIFT") then translate(player, {0.0, 0.25, 0.0}, "local") end
+		if isKeyPressed("W") then 
+			translate(player, {0.0, 0.5, 0.0}, "local")
+		end
+		
+
+		if isKeyPressed("S") then 
+			translate(player, {0.0, -0.5, 0.0}, "local") 
+		end
+		
+
+		if isKeyPressed("A") then 
+			translate(player, {-0.5, 0.0, 0.0}, "local") 
+		end
+		
+
+		if isKeyPressed("D") then 
+			translate(player, {0.5, 0.0, 0.0}, "local") 
+		end
+		
+
+		if isKeyPressed("LSHIFT") then 
+			translate(player, {0.0, 0.25, 0.0}, "local") 
+		end
 	-- end
 
 	if isKeyPressed("SPACE") then 
 
     	if isCollisionBetween(player, pblock) == true then 
     		-- translate(player, {0.0, 0.0, 2.0}, "local")
-    		addCentralForce(player, {0.0, 0.0, 2000.0}, "global")
+    		addCentralForce(player, {0.0, 0.0, 1000}, "local")
     	end
 	end
 
-    setGravity({0.0, 0.0, 0.0})
-
-    if isCollisionBetween(player, pblock) ~= true then 
-    	addCentralForce(player, {0.0, 0.0, -100.0}, "global")
+    if isCollisionBetween(player, pblock) == false then
+    	addCentralForce(player, {0.0, 0.0, -50}, "local")
     end
+    
+
+    -- if isCollisionBetween(player, pblock) ~= true then 
+    -- 	addCentralForce(player, {0.0, 0.0, -100.0}, "global")
+    -- end
 
 	-- if isKeyPressed("W") then addCentralForce(player, {0.0, 2000.0, 0.0}, "local") end
 	-- if isKeyPressed("LSHIFT") then addCentralForce(player, {0.0, 2000.0, 0.0}, "local") end
